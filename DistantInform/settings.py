7ed53b5ce043461a28cee99189d904b9c1bc3b92
Erraida <75965660+Erraida@users.noreply.github.com>
2021-04-08 21:11:35 +0300

    Update settings.py

--- DistantInform/settings.py
+++ DistantInform/settings.py
@@ -173,5 +173,7 @@ from .ckeditor_settings import *
 CELERY_TIMEZONE = "Europe/Moscow"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL=os.environ['REDIS_URL']
+CELERY_RESULT_BACKEND=os.environ['REDIS_URL']
+#CELERY_BROKER_URL = 'redis://localhost:6379/0'
+#CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
