0519d34dd0f0794b260397270fb4959cc06d891a
Erraida <75965660+Erraida@users.noreply.github.com>
2021-04-08 20:53:26 +0300

    Update settings.py

--- DistantInform/settings.py
+++ DistantInform/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     }
 }"""
 ## ------postgresql--------
- DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
