ea03316a2c5171b32e6e9f804c849f876d52b6ca
Erraida <75965660+Erraida@users.noreply.github.com>
2021-04-15 17:31:25 +0300

    Update settings.py

--- DistantInform/settings.py
+++ DistantInform/settings.py
@@ -177,8 +177,8 @@ from .ckeditor_settings import *
 CELERY_TIMEZONE = "Europe/Moscow"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.environ['REDIS_URL']
+CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
