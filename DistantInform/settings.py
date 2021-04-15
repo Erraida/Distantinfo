a700de715e81291d658e6d8069dcbbf08fcc7859
Erraida <75965660+Erraida@users.noreply.github.com>
2021-04-15 18:03:23 +0300

    Update settings.py

--- DistantInform/settings.py
+++ DistantInform/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': 'os.environ['DB_HOST'],
+        'HOST': os.environ['DB_HOST'],
         'PORT': '5432',
     }
  }
