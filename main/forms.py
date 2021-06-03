e2a8d9aac98404f63261a812d84c67d89d6ac998
Erraida <75965660+Erraida@users.noreply.github.com>
2021-06-03 20:22:11 +0300

    Update forms.py

--- main/forms.py
+++ main/forms.py
@@ -48,19 +48,20 @@ class LectReqForm (ModelForm):
             self.fields['Discipline'].label = "Дисциплина"
 
 
-class CommentForm (ModelForm):
-
-    comment_text = forms.Textarea(attrs={
-        'id': 'comment',
-        'placeholder': 'Название'
-    })
+class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ['comment_text']
+        widgets = {
+            'comment_text': forms.Textarea(
+                attrs={'id': 'comment',
+                       'placeholder': 'Введитк коментарий'
+                       }),
+        }
 
     def __init__(self, *args, **kwargs):
-            super(CommentForm, self).__init__(*args, **kwargs)
-            self.fields['comment_text'].label = ""
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields['comment_text'].label = ""
 
 
 
